3f60cd3d47d12fabbe39a1f2c169a34c151e4c9b
Hu, Xiaohui <xiaohuhu@stubhub.com>
2020-11-03 11:13:57 +0800

    dotenv for NodeJS

--- basic-env/server/index.js
+++ basic-env/server/index.js
@@ -2,12 +2,16 @@ const path = require('path');
 const express = require('express');
 const uiAppStatic = path.resolve(__dirname, '../build');
 
+require('dotenv').config({ path: path.resolve(__dirname, '../.env.server') });
+
 const PORT = process.env.PORT || 3006;
 const app = express();
+
 
 app.get('/', (req, resp) => {
     console.log(process.env.NODE_ENV);
     console.log(process.env.PORT);
+    console.log(process.env.API_URI);
     resp.sendFile('index.html', { root: uiAppStatic });
   });
   
